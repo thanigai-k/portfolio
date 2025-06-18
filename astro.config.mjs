948ae71e4d156153bfdc44e4bda6badb8e23cc98
thanigai-k <inbox.thanigai@gmail.com>
2025-06-18 20:19:12 +0530

    config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://thanigai-k.github.io",
-  base: "portfolio-site",
+  base: "portfolio",
   integrations: [mdx(), sitemap(), react(), icon()],
   vite: {
     plugins: [tailwindcss()],
